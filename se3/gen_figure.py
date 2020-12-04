db7dadd916afa31c7616bfb51f88932f84cc0042
Kalin N <kalinnorman@gmail.com>
2020-12-03 17:13:11 -0700

    Some minor changes to modify title positionings and noise values

--- se3/gen_figure.py
+++ se3/gen_figure.py
@@ -6,8 +6,8 @@ from mpl_toolkits.mplot3d import Axes3D
 
 if __name__ == "__main__":
     # setup system
-    Q = np.diag([.001, .001, .001, 0, 0, 0, .001, .001, .001])
-    R = np.diag([.001, .001, .001])
+    Q = np.diag([.01, .01, .01, 0, 0, 0, .01, .01, .01])
+    R = np.diag([.01, .01, .01])
     # Specify filenames of datasets to use
     filenames = ["run1.npz", "run2.npz", "run3.npz"]
 
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     ax1 = fig.add_subplot(131, projection='3d')
     ax2 = fig.add_subplot(132, projection='3d')
     ax3 = fig.add_subplot(133, projection='3d')
-    fig.suptitle("SE(3) IEKF Simulations")
-    ax1.set_title("(a)")
-    ax2.set_title("(b)")
-    ax3.set_title("(c)")
+    fig.suptitle("SE(3) IEKF Simulations", y=0.78)
+    ax1.set_title("(a)", y=0.98)
+    ax2.set_title("(b)", y=0.98)
+    ax3.set_title("(c)", y=0.98)
     ax = [ax1, ax2, ax3]
 
     for i in range(len(filenames)):
@@ -46,8 +46,8 @@ if __name__ == "__main__":
         ax[i].plot(x[:,0,4], x[:,1,4], x[:,2,4], label="Actual")
         ax[i].plot(x_unfilt[:,0,4], x_unfilt[:,1,4], x_unfilt[:,2,4], label="Unfiltered")
         ax[i].plot(x_result[:,0,4], x_result[:,1,4], x_result[:,2,4], label="iEKF")
-        ax[i].legend()
-    
+        # ax[i].legend()
+    ax[0].legend()
     # plt.show()
     plt.savefig("figure.pdf", bbox_inches='tight')
 
